5be55a92bf5a13adf401775404bc150eba6d1772
Abdulaziz-06 <aaziz871048@gmail.com>
2025-06-03 10:07:36 +0530

    updated

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.get("/d",(req,res)=>{
 })
 
 const products = [
-  { id: 1,name:"Product 1",price:100},
-  { id: 2,name:"Product 2",price:200},
-  { id: 3,name:"Product 3",price:300}
+  { id: 1,name:"Product 1",price:1000},
+  { id: 2,name:"Product 2",price:2000},
+  { id: 3,name:"Product 3",price:3000}
 ];
 app.get("/products",(req,res)=>{
   res.send(products);
